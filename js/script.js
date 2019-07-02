269902bd1dd94f10a6a2d9996d6e7f72bc2588cd
LivHed <LivHed@github.com>
2019-07-02 15:07:47 +0000

    Add methods for hiding panels

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
- // put your code here
+    
+     //hides all panels when a panel is clicked
+ $(".theButton").click(function(){
+   $("#panel .container").siblings().hide();
+   }) 
+   
+   //hides only the panel that was clicked
+  $(".theButton").click(function(){
+  $(this).hide();
+   })
+   
+   
+   
 });
